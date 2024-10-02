be726796d9223c98eb8fd79a8be8181fec0c8533
Marc Romeyn <mromeijn@nvidia.com>
2024-10-02 16:53:57 +0200

    Fix failing tests
    Signed-off-by: Marc Romeyn <mromeijn@nvidia.com>

--- test/cli/test_api.py
+++ test/cli/test_api.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from configparser import ConfigParser
 from dataclasses import dataclass
 from test.dummy_factory import DummyModel, dummy_entrypoint
