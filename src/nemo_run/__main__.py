1dc27a64d9194697e6edccae23593740d2a70f05
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-18 15:59:47 -0700

    Ignore Deprecation/Future warnings
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- src/nemo_run/__main__.py
+++ src/nemo_run/__main__.py
@@ -12,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import warnings
+
+warnings.simplefilter(action="ignore", category=DeprecationWarning)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 from nemo_run.cli.api import create_cli
 
