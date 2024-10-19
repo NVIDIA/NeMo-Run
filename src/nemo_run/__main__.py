0e04dfeabe2c4ebd8a8fc21570866ef860b1c985
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-19 16:43:19 -0700

    noqa
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- src/nemo_run/__main__.py
+++ src/nemo_run/__main__.py
@@ -17,6 +17,6 @@ import warnings
 warnings.simplefilter(action="ignore", category=DeprecationWarning)
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-from nemo_run.cli.api import create_cli
+from nemo_run.cli.api import create_cli  # noqa: E402
 
 app = create_cli()
