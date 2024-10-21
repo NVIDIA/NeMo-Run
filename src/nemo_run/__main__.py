281f9b0804f13b5811b44e43d88306db5e8e33c8
Alexandros Koumparoulis <akoumparouli@nvidia.com>
2024-10-21 11:00:51 -0700

    add NEMORUN_SHOW_WARNINGS
    Signed-off-by: Alexandros Koumparoulis <akoumparouli@nvidia.com>

--- src/nemo_run/__main__.py
+++ src/nemo_run/__main__.py
@@ -12,10 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 import warnings
 
-warnings.simplefilter(action="ignore", category=DeprecationWarning)
-warnings.simplefilter(action="ignore", category=FutureWarning)
+if not 'NEMORUN_SHOW_WARNINGS' in os.environ:
+    warnings.simplefilter(action="ignore", category=DeprecationWarning)
+    warnings.simplefilter(action="ignore", category=FutureWarning)
 
 from nemo_run.cli.api import create_cli  # noqa: E402
 
