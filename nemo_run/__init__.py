9f6a6c4b3ad04360e620058ac8d7ced79fe7e56f
oliver könig <okoenig@nvidia.com>
2025-03-06 01:35:45 +0100

    f
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- nemo_run/__init__.py
+++ nemo_run/__init__.py
@@ -13,28 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from nemo_run import cli
 from nemo_run.api import autoconvert, dryrun_fn
 from nemo_run.config import Config, ConfigurableMixin, Partial, Script
-from nemo_run.core.execution.base import (
-    Executor,
-    ExecutorMacros,
-    import_executor,
-)
+from nemo_run.core.execution.base import (Executor, ExecutorMacros,
+                                          import_executor)
 from nemo_run.core.execution.dgxcloud import DGXCloudExecutor
 from nemo_run.core.execution.docker import DockerExecutor
-from nemo_run.core.execution.launcher import FaultTolerance, SlurmRay, SlurmTemplate, Torchrun
+from nemo_run.core.execution.launcher import (FaultTolerance, SlurmRay,
+                                              SlurmTemplate, Torchrun)
 from nemo_run.core.execution.local import LocalExecutor
 from nemo_run.core.execution.skypilot import SkypilotExecutor
 from nemo_run.core.execution.slurm import SlurmExecutor
-from nemo_run.core.packaging import GitArchivePackager, HybridPackager, Packager, PatternPackager
+from nemo_run.core.packaging import (GitArchivePackager, HybridPackager,
+                                     Packager, PatternPackager)
 from nemo_run.core.tunnel.client import LocalTunnel, SSHTunnel
 from nemo_run.devspace.base import DevSpace
 from nemo_run.help import help
 from nemo_run.lazy import LazyEntrypoint, lazy_imports
+from nemo_run.package_info import __package_name__, __version__
 from nemo_run.run.api import run
 from nemo_run.run.experiment import Experiment
 from nemo_run.run.plugin import ExperimentPlugin as Plugin
+
+from nemo_run import cli
 
 __all__ = [
     "autoconvert",
@@ -70,9 +71,6 @@ __all__ = [
     "Torchrun",
     "SlurmRay",
     "SlurmTemplate",
+    "__version__",
+    "__package_name__",
 ]
-
-try:
-    from nemo_run._version import __version__
-except Exception:
-    __version__ = "0.0.1"
